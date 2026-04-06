66cdc3bfdf48d655e44d59820e2dccb1d7e219d8
Vi <71043551+Vizereth@users.noreply.github.com>
2026-04-06 21:04:48 +0800

    add: new lvl

--- src/pacman/config/levels.ts
+++ src/pacman/config/levels.ts
@@ -1,12 +1,18 @@
 import { type LevelConfigType } from "../types.js";
-import { MAP_LVL_1 } from "./maps.js";
+import { LEVEL_2_MAP, LEVEL_1_MAP } from "./maps.js"; // 🌟 Импортируем новую карту
 
 const LEVEL_CONFIGS: Record<number, LevelConfigType> = {
   1: {
-    map: MAP_LVL_1,
-    mapColor: "#120a8f",
+    map: LEVEL_1_MAP,
+    mapColor: "#120a8f", // Синий
     buffDuration: 10,
     buffThreshold: 3,
+  },
+  2: {
+    map: LEVEL_2_MAP, // 🌟 Твоя новая карта
+    mapColor: "#004d40", // Тёмно-зелёный
+    buffDuration: 7, // 🌟 Чуть меньше времени на поедание привидений
+    buffThreshold: 2, // 🌟 Предупреждение о мигании сработает за 2 секунды
   },
 };
 
