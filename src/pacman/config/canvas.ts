0ea4ac33116f12a2c50fb6bf7d3defee02547c7c
Vi <71043551+Vizereth@users.noreply.github.com>
2026-04-07 23:31:05 +0800

    removed ui cvs

--- src/pacman/config/canvas.ts
+++ src/pacman/config/canvas.ts
@@ -1,15 +1,13 @@
 const CANVAS_CONFIG = {
   tile: {
     size: 22, // Size of each tile in pixels
-    extraHeightFactor: 6, // Extra height as a multiplier of tile size
   },
   canvasIds: {
     map: "map-cvs",
     food: "food-cvs",
     pill: "pill-cvs",
     pacman: "pacman-cvs",
-    ui: "ui-cvs",
-    ghosts: "ghosts-cvs",
+    ghosts: "ghosts-cvs", // 🌟 Notice "ui" is gone!
   },
 };
 
