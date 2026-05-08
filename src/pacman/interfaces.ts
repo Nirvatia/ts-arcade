9ec69fb513992e57882a12824301ee953355400b
Vi <71043551+Vizereth@users.noreply.github.com>
2026-05-08 14:10:20 +0800

    add: empty files

---
+++ src/pacman/interfaces.ts
@@ -0,0 +1,16 @@
+// src/core/interfaces/Drawable.ts
+export interface Drawable {
+  readonly needsRedraw: boolean;
+  draw(animate: boolean, dt?: number): void;
+  requestRedraw(): void;
+}
+
+export interface Updatable extends Drawable {
+  update(dt: number): void;
+}
+
+// src/core/interfaces/Collectible.ts
+export interface Collectible {
+  spawn(): void;
+  eat(i: number, j: number): void;
+}
